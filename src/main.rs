527ce2c1e322924f279f19220c9c208da211e2df
kakolla <abhishekkakolla1@gmail.com>
2026-06-28 22:40:42 -0700

    read text and render to screen

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use macroquad::prelude::*;
 #[macroquad::main("Type")]
 async fn main() {
     request_new_screen_size(1920.0, 1080.0);
-    let mut height = 0.0;
-    let mut width = 0.0;
+    let mut height;
+    let mut width;
     request_new_screen_size(1920.0, 1080.0);
+
+    // read text
+    let text = include_str!("texts/first.txt");
 
     loop {
         height = screen_height();
@@ -14,7 +17,7 @@ async fn main() {
         // println!("{} {} ", width, height);
 
         // draw_line(40.0, 40.0, 100.0, 150.0, 60.3, BLUE);
-        draw_text("welcome", width / 2.0, height / 2.0, 48.0, WHITE);
+        draw_text(&text, width / 2.0, height / 2.0, 48.0, WHITE);
         next_frame().await;
     }
 }
